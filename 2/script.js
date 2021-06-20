37697b0cf06f4edd86e8592bdc18ad0554811e70
kirill1991okt <kirill1991okt@gmail.com>
2021-06-20 14:34:02 +0300

    fix slider

--- 2/script.js
+++ 2/script.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-    $('.language').on('click', function() {
-        if($(this).hasClass('open')) {
+$(document).ready(function () {
+    $('.language').on('click', function () {
+        if ($(this).hasClass('open')) {
             $(this).removeClass('open');
         } else {
             $(this).addClass('open');
         }
     });
-    $('.language .popup div').on('click', function(e) {
+    $('.language .popup div').on('click', function (e) {
         e.stopPropagation();
         // change language code
     });
 
 
-    $('.popup').mouseleave(function() {
+    $('.popup').mouseleave(function () {
         $('.language').removeClass('open');
     })
 
@@ -23,14 +23,22 @@ $(document).ready(function() {
 function squareScroll($el) {
     var maxHp = 100 / ($(document).height() - $(window).height() + $el.height() * 2);
     var pre = $(document).scrollTop();
-    $(document).scroll(function() {
+    $(document).scroll(function () {
         var cScroll = $(this).scrollTop();
         var p = cScroll * maxHp / 20;
-        if(cScroll > pre) {
-            $el.css({'top': $el.position().top + p + 'px'});
+        if (cScroll > pre) {
+            $el.css({
+                'top': $el.position().top + p + 'px'
+            });
         } else {
-            $el.css({'top': $el.position().top - p + 'px'});
+            $el.css({
+                'top': $el.position().top - p + 'px'
+            });
         }
         pre = cScroll;
     });
 }
+
+$("#carouselExampleControls").carousel({
+    interval: false
+});
